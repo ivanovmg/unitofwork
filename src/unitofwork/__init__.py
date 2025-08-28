807c364c601c37ad5afd5e77b690c889172a30d8
Maxim Ivanov <ivanovmg@gmail.com>
2025-08-28 17:51:37 +0700

    Setup null handler for logger

--- src/unitofwork/__init__.py
+++ src/unitofwork/__init__.py
@@ -1,7 +1,14 @@
 # Copyright (c) 2025 Maxim Ivanov
 # SPDX-License-Identifier: MIT
 
+import logging
+
 from .uow import RollbackError, UnitOfWork, UnitOfWorkError
+
+
+# Set up null handler for the library's logger
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 __all__ = [
